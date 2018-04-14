af5f181849143eb5b549fa8d3eefa22dd6c951ba
Lewis Gavin <lewisgavin@lewiss-imac.local>
2018-04-14 17:53:38 +0100

    Initial Commit

---
+++ fourwordreviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fourwordreviews
+//
+//  Created by Lewis Gavin on 14/04/2018.
+//  Copyright © 2018 Lewis Gavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
