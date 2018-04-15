3cafa757874570a29d503949f8cfaa1af3c6c580
Lewis Gavin <lewisgavin@lewiss-imac.local>
2018-04-15 21:03:49 +0100

    Added button with functionality to send data to REST API

--- fourwordreviews/ViewController.swift
+++ fourwordreviews/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var word1TextField: UITextField!
+    @IBOutlet weak var word2TextField: UITextField!
+    @IBOutlet weak var word3TextField: UITextField!
+    @IBOutlet weak var word4TextField: UITextField!
+    @IBOutlet weak var beerNameLabel: UILabel!
+    @IBOutlet weak var fourWordReviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    @IBAction func addReviewButton(_ sender: UIButton) {
+        
+        let postBody = ["product_name": nameTextField.text, "word1": word1TextField.text, "word2": word2TextField.text, "word3": word3TextField.text, "word4": word4TextField.text]
+        
+        let reviewEndpoint = URL(string: "http://localhost:3000/api/v1/reviews")!
+        var request = URLRequest(url: reviewEndpoint)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: postBody, options: [])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request)
+        task.resume()
+        
+    }
+    
 }
 
