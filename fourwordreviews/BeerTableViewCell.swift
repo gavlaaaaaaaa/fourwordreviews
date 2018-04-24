96fccc791e8a748976dd4062e8f825866ad1002c
Lewis Gavin <lewisgavin@lewiss-imac.local>
2018-04-24 21:50:22 +0100

    Added list page and started dev for retrieving beer reviews from API

---
+++ fourwordreviews/BeerTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  BeerTableViewCell.swift
+//  fourwordreviews
+//
+//  Created by Lewis Gavin on 24/04/2018.
+//  Copyright © 2018 Lewis Gavin. All rights reserved.
+//
+
+import UIKit
+
+class BeerTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var fourWordReviewLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
