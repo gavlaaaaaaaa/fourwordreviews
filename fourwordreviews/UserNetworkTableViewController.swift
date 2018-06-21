25d88801eb1dfd117180440804bd264063bc5031
gavlaaaaaaaa <gavlaaaaa5@gmail.com>
2018-06-21 22:26:42 +0100

    fixed format of follow lists and started unfollow functionality

--- fourwordreviews/UserNetworkTableViewController.swift
+++ fourwordreviews/UserNetworkTableViewController.swift
@@ -16,6 +16,7 @@ class UserNetworkTableViewController: UITableViewController  {
     var followers:[User] = []
     var following:[User] = []
     
+    let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,8 +61,18 @@ class UserNetworkTableViewController: UITableViewController  {
         {
         case 0:
             cell.usernameLabel.text = following[indexPath.row].username
+            cell.followButton.isEnabled = true
+            cell.followButton.setTitle("Unfollow", for: UIControlState.normal)
         case 1:
             cell.usernameLabel.text = followers[indexPath.row].username
+            if following.contains(followers[indexPath.row]){
+                cell.followButton.setTitle("Follow Back", for: UIControlState.normal)
+                cell.followButton.isEnabled = true
+            }
+            else {
+                cell.followButton.setTitle("Following", for: UIControlState.normal)
+                cell.followButton.isEnabled = false
+            }
         default:
             break
         }
@@ -76,10 +87,33 @@ class UserNetworkTableViewController: UITableViewController  {
         self.tableView.reloadData()
     }
     
+    @IBAction func followButtonPressed(_ sender: AnyObject) {
+        let senderCell = sender.superview??.superview as! UserNetworkTableViewCell
+        switch(segmentControl.selectedSegmentIndex)
+        {
+        case 0:
+            unfollowUser(userId: following[(self.tableView.indexPath(for: senderCell)?.row)!].id!) {(result) -> () in
+                if result {
+                    //TODO FIX THIS SO IT RUNS ON MAIN THREAD
+                    //TODO UNFOLLOW API NOT WORKING
+                    senderCell.followButton.setTitle("Follow", for: UIControlState.normal)
+                    senderCell.followButton.isEnabled = true
+                }
+            }
+            break
+        case 1:
+            //returnVal = followers.count
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    
     //MARK - Helper functions
     
     func loadFollowers(userId: Int){
-        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
         let followersEndpoint = URL(string: "http://localhost:3000/api/v1/followers/"+String(userId))!
         let task = session.dataTask(with: followersEndpoint) {(data: Data?, response: URLResponse?, error: Error?) in
             guard let data = data else {
@@ -101,9 +135,8 @@ class UserNetworkTableViewController: UITableViewController  {
     }
     
     func loadFollowing(userId: Int){
-        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
-        let followersEndpoint = URL(string: "http://localhost:3000/api/v1/following/"+String(userId))!
-        let task = session.dataTask(with: followersEndpoint) {(data: Data?, response: URLResponse?, error: Error?) in
+        let followingEndpoint = URL(string: "http://localhost:3000/api/v1/following/"+String(userId))!
+        let task = session.dataTask(with: followingEndpoint) {(data: Data?, response: URLResponse?, error: Error?) in
             guard let data = data else {
                 //completion(nil)
                 return
@@ -122,8 +155,22 @@ class UserNetworkTableViewController: UITableViewController  {
         
     }
     
-
-    
+    func unfollowUser(userId: Int, completion: @escaping (_ result: Bool)->()){
+        let unfollowEndpoint = URL(string: "http://localhost:3000/api/v1/following/"+String(UserSingleton.sharedInstance.user_id)+"/unfollow/"+String(userId))!
+        var request = URLRequest(url: unfollowEndpoint)
+        request.httpMethod = "DELETE"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request, completionHandler: {(data, response, error) -> Void in
+            if let response = response as? HTTPURLResponse , 200...299 ~= response.statusCode {
+                completion(true)
+            } else {
+                completion(false)
+            }
+            
+        })
+        task.resume()
+    }
     
     
     
